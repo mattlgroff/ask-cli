35828acee924c0168576b2d277a46883dc35c1b1
Matt Groff <mattlgroff@gmail.com>
2023-05-12 14:10:25 -0400

    Init repo

---
+++ README.md
@@ -0,0 +1,34 @@
+### Ask OpenAI GPT from the bash command line
+
+# Instructions
+In my `~/.bashrc` I added the following lines:
+```bash
+export OPEN_AI_API_KEY="myopenaiapikey"
+
+alias ask='ruby ~/ask/ask.rb'
+```
+
+Then I run `source ~/.bashrc` and I can use the `ask` command from the command line. I have my OpenAI API key in the environment variable `OPEN_AI_API_KEY`. The location of your `ask.rb` file may be different, but I put mine in my home directory in a folder called `ask`.
+
+# Usage
+```bash
+  ask "How do I extract my discord file?"
+```
+
+This small program will pass OpenAI the contents of `pwd` and `ls -al` so it has more context about what you're asking and can give you a better answer based on the context of where you called the file in your terminal.
+
+In this example you can see how the extra context is helpful:
+```bash
+matt@ubuntu-desktop:~$ ls
+ask  Desktop  discord.tar.gz  Documents  Downloads  Music  Pictures  Public  snap  Templates  Videos  working
+matt@ubuntu-desktop:~$ ask "How do I extract my discord file?"
+You can extract your discord file using the following command:
+
+tar -xvf discord.tar.gz
+
+This will extract the contents of the `discord.tar.gz` file in the current directory.
+```
+
+Pretty nifty!
+
+I have it set to use gpt-3.5-turbo to say on costs, but you can change it in the `ask.rb` file to `gpt-4` if you so desire. I find with the context and command line questions gpt-3.5-turbo is more than enough.
---
+++ ask.rb
@@ -0,0 +1,61 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'thread'
+
+def ask_openai(query)
+  pwd = `pwd`.strip
+  ls = `ls -al`.gsub(/\s+/, ' ').strip
+  
+  loading_thread = Thread.new do
+    i = 0
+    while true
+      print "\rLoading" + "." * ((i % 3) + 1) + " " * (3 - i)
+      sleep 0.5
+      i = (i + 1) % 3
+    end
+  end
+
+  uri = URI.parse("https://api.openai.com/v1/chat/completions")
+  request = Net::HTTP::Post.new(uri)
+  request["Authorization"] = "Bearer #{ENV['OPEN_AI_API_KEY']}"
+  request["Content-Type"] = "application/json"
+  request.body = JSON.dump({
+    "model" => "gpt-3.5-turbo",
+    "messages" => [
+      {
+        "role" => "system",
+        "content" => "You are a helpful assistant being asked questions from a command line interface. The current directory is #{pwd} and its contents are: #{ls}"
+      },
+      {
+        "role" => "user",
+        "content" => query
+      }
+    ],
+    "temperature" => 0.2
+  })
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+
+  loading_thread.kill
+  print "\r" + (" " * 20) + "\r" # Clear the loading message
+
+  if response.code == '200'
+    body = JSON.parse(response.body)
+    content = body['choices'][0]['message']['content'].strip
+    # Remove triple backticks and highlight the content inside them
+    content.gsub!(/```(.*?)```/m) { "\e[32m#{$1.strip}\e[0m" }
+    print content
+    print "\n"
+  else
+    puts "Error: #{response.code}"
+  end
+end
+
+ask_openai(ARGV[0])
