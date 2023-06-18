d77889571558809cde3d6fbd299833cdbfb90b4f
Matt Groff <matt@umbrage.com>
2023-06-17 20:19:45 -0400

    Use function calling to only provide ls and pwd in context when specifically asked for it

--- ask.rb
+++ ask.rb
@@ -3,36 +3,56 @@ require 'uri'
 require 'json'
 require 'thread'
 
-def ask_openai(query)
-  pwd = `pwd`.strip
-  ls = `ls -al`.gsub(/\s+/, ' ').strip
-  
-  loading_thread = Thread.new do
-    i = 0
-    loop do
-      print "\rLoading" + "." * ((i % 3) + 1) + " " * (3 - i)
-      sleep 0.5
-      i = (i + 1) % 3
-    end
-  end
+def pwd
+  `pwd`.strip
+end
+
+def ls
+  `ls -al`.gsub(/\s+/, ' ').strip
+end
+
+def make_openai_request(query, previous_messages = [])
+  function_definitions = [
+    {
+      "name" => "pwd",
+      "description" => "Get the current directory",
+      "parameters" => {
+        "type" => "object",
+        "properties" => {}
+      },
+    },
+    {
+      "name" => "ls",
+      "description" => "Get the contents of the current directory",
+      "parameters" => {
+        "type" => "object",
+        "properties" => {}
+      },
+    },
+  ]
+
+  messages = [
+    {
+      "role" => "system",
+      "content" => "You are a helpful assistant being asked questions from a command line interface."
+    },
+    {
+      "role" => "user",
+      "content" => query
+    }
+  ] + previous_messages
 
   uri = URI.parse("https://api.openai.com/v1/chat/completions")
   request = Net::HTTP::Post.new(uri)
   request["Authorization"] = "Bearer #{ENV['OPEN_AI_API_KEY']}"
   request["Content-Type"] = "application/json"
   request.body = {
-    "model" => "gpt-3.5-turbo",
-    "messages" => [
-      {
-        "role" => "system",
-        "content" => "You are a helpful assistant being asked questions from a command line interface. The current directory is #{pwd} and its contents are: #{ls}"
-      },
-      {
-        "role" => "user",
-        "content" => query
-      }
-    ],
-    "temperature" => 0.2
+    "model" => "gpt-3.5-turbo-0613",
+    "messages" => messages,
+    "functions" => function_definitions,
+    "function_call" => "auto",
+    "temperature" => 0.2,
+    "stream" => false,
   }.to_json
 
   req_options = {
@@ -43,18 +63,44 @@ def ask_openai(query)
     http.request(request)
   end
 
-  loading_thread.kill
-  print "\r#{' ' * 20}\r" # Clear the loading message
+  response
+end
 
+def process_openai_response(response, query, previous_messages = [])
   if response.code == '200'
     body = JSON.parse(response.body)
-    content = body['choices'][0]['message']['content'].strip
-    # Remove triple backticks and highlight the content inside them
-    content.gsub!(/```(.*?)```/m) { "\e[32m#{$1.strip}\e[0m" }
-    puts content
+    last_message = body['choices'][0]['message']
+
+    if last_message['function_call']
+      case last_message['function_call']['name']
+      when 'pwd'
+        puts "Calling function: pwd"
+        function_response = pwd
+        previous_messages += [{"role" => "function", "name" => "pwd", "content" => function_response}]
+        response = make_openai_request(query, previous_messages)
+        process_openai_response(response, query, previous_messages)
+      when 'ls'
+        puts "Calling function: ls"
+        function_response = ls
+        previous_messages += [{"role" => "function", "name" => "ls", "content" => function_response}]
+        response = make_openai_request(query, previous_messages)
+        process_openai_response(response, query, previous_messages)
+      else
+        raise "Unknown function: #{last_message['function_call']['name']}"
+      end
+    elsif last_message['content']
+      content = last_message['content'].strip
+      content.gsub!(/```(.*?)```/m) { "\e[32m#{$1.strip}\e[0m" }
+      puts content
+    else
+      puts "Error: Unexpected message from assistant: #{last_message}"
+    end
   else
     puts "Error: #{response.code}"
+    puts "Response body:"
+    puts response.body
   end
 end
 
-ask_openai(ARGV[0])
+response = make_openai_request(ARGV[0])
+process_openai_response(response, ARGV[0])
